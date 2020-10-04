bc458ea8d03158a5b306b7b547de461138c2df72
Koichi Shiraishi <zchee.io@gmail.com>
2020-10-05 03:51:34 +0900

    controllers/timeleap: refactoring and add documentations

--- controllers/timeleap/timeleap_controller.go
+++ controllers/timeleap/timeleap_controller.go
@@ -8,13 +8,15 @@ import (
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	timeleapv1alpha1 "github.com/zchee/kube-timeleap/apis/timeleap/v1alpha1"
 )
 
-// TimeLeapReconciler reconciles a TimeLeap object
+// TimeLeapReconciler reconciles a TimeLeap object.
 type TimeLeapReconciler struct {
 	client.Client
 	client.Reader
@@ -22,21 +24,23 @@ type TimeLeapReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+// compile time check whether the TimeLeapReconciler implements ctrlreconcile.Reconciler interface.
+var _ reconcile.Reconciler = (*TimeLeapReconciler)(nil)
+
 // +kubebuilder:rbac:groups=timeleap.x-k8s.io,resources=timeleaps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=timeleap.x-k8s.io,resources=timeleaps/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=timeleap.x-k8s.io,resources=timeleaps/finalizers,verbs=update
 
-func (r *TimeLeapReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
+// Reconcile implements a reconcile.Reconciler.
+func (r *TimeLeapReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	_ = r.Log.WithValues("timeleap", req.NamespacedName)
 
-	// your logic here
-
-	return ctrl.Result{}, nil
+	return reconcile.Result{}, nil
 }
 
-func (r *TimeLeapReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
+// SetupWithManager setups the Controller with manager.Manager.
+func (r *TimeLeapReconciler) SetupWithManager(mgr manager.Manager) error {
+	return builder.ControllerManagedBy(mgr).
 		For(&timeleapv1alpha1.TimeLeap{}).
 		Complete(r)
 }
